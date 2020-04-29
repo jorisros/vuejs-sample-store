711e0fcb32d6e339198e25276529b18887153a5e
Joris Ros <joris.ros@gmail.com>
2020-04-30 00:08:22 +0200

    Add Vuex to the project

---
+++ src/components/ComponentB.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ComponentB"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import storeData from './store'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store(storeData)
 
 new Vue({
+  store: store,
   render: h => h(App),
 }).$mount('#app')
