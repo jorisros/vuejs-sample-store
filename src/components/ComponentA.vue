948fa97129106aa2cb636022283b5576f43a5ab0
Joris Ros <joris.ros@gmail.com>
2020-04-30 11:51:39 +0200

    Add component A

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button v-on:click="toggle">Press A</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ComponentA",
+        methods: {
+            toggle: function (event) {
+                console.log(event);
+                this.$store.commit('startLoading');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
